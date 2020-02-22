ddee89b260f75b771b1f1b310baef32d0e51ac21
Julien Bourdic <julien@nixa.ca>
2020-02-22 03:33:58 -0500

    User auth

---
+++ api/src/Controller/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+class AuthController extends AbstractController
+{
+    /** @var UserRepository $userRepository */
+    private $userRepository;
+
+    /**
+     * AuthController Constructor
+     *
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+    /**
+     * @Route("/auth", name="auth")
+     */
+    public function index()
+    {
+        return $this->render('auth/index.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+
+    /**
+     * Register new user
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @Route("/register", methods={"POST"})
+     */
+    public function register(Request $request)
+    {
+        $newUserData['email']    = $request->get('email');
+        $newUserData['password'] = $request->get('password');
+
+        $user = $this->userRepository->createNewUser($newUserData);
+
+        if (!!$user === false) {
+            return new Response('User creation problem', 500);
+        }
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+}
---
+++ api/src/Repository/UserRepository.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Couchbase\UserSettings;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(ManagerRegistry $registry, UserPasswordEncoderInterface $encoder)
+    {
+        parent::__construct($registry, User::class);
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newEncodedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    public function createNewUser(array $data): User
+    {
+        $user = new User();
+        $user->setEmail($data['email'])
+            ->setPassword($this->encoder->encodePassword($user, $data['password']));
+
+        try {
+            $this->_em->persist($user);
+            $this->_em->flush();
+        } catch (ORMException $exception) {
+            return null;
+        }
+
+        return $user;
+    }
+    // /**
+    //  * @return User[] Returns an array of User objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?User
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
